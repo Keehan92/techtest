5b1ef6c992ffe81c62dd2b877ad0abc86d46cb99
KeeHan <keehan_92@hotmail.com>
2023-11-11 10:39:59 +0800

    initial commit

---
+++ src/main/java/com/example/techtest/TechtestApplication.java
@@ -0,0 +1,23 @@
+package com.example.techtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+public class TechtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechtestApplication.class, args);
+    }
+    
+}
+
+
